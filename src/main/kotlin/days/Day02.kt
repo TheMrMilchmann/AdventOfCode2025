ed5b8a2e66df2f72f4ec64a27ece1de89a04dbe6
Leon Linhart <themrmilchmann@gmail.com>
2025-12-02 10:13:22 +0100

    feat(02): part 1

---
+++ src/main/kotlin/days/Day02.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2025 Leon Linhart
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package days
+
+import utils.readInput
+
+fun main() {
+    val ranges = readInput()
+        .first()
+        .split(',')
+        .map {
+            val (start, end) = it.split('-')
+            (start.toLong())..(end.toLong())
+        }
+
+    val r = "^(.+)\\1$".toRegex()
+
+    println("Part 1: ${ranges.flatten().filter { it.toString().matches(r) }.onEach { println(it) }.sum()}")
+}
